4593bc59785011404cd056c90ed19f7f6f91971f
ershelaruvina <ershelaruvina@192.168.1.13>
2016-07-11 17:52:22 -0400

    The duplicates were removed.

--- prep/src/main/java/com/leadway/prep/Palindrome.java
+++ prep/src/main/java/com/leadway/prep/Palindrome.java
@@ -8,48 +8,37 @@ public class Palindrome {
 
 	 public static void main(String[] args) {
 		 scnr = new Scanner (System.in);
-		 String temp = "";
 		System.out.println("Enter a word:");
 		String word = scnr.next();	
-		if (isPalindrome(word) == true) {
-			System.out.println("'" + word + "'" + " is a palindrome!!");
-			temp = word;
-		} else{
-			System.out.println("'" + word + "'"+ " is NOT a palindrome!!");
-		temp = word;
-			
+		
+			  do{
+				 if (word.equals("exit")){
+					 break;
+				 }
+				 else{
+					 
+	               if (isPalindrome(word)) {
+						System.out.println("'" + word + "'" + " is a palindrome!!");
+					} 
+	               else{
+						System.out.println("'" + word + "'"+ " is NOT a palindrome!!");
+					}  
+				 }
+				  word = scnr.next();	  
+			  } while (word != "exit"); 
 		}
-		 while (temp != "exit"){
-			 
-			  word = scnr.next();	
-			  if (isPalindrome(word) == true) {
-					System.out.println("'" + word + "'" + " is a palindrome!!");
-					temp = word;
-				} else{
-					System.out.println("'" + word + "'"+ " is NOT a palindrome!!");
-					temp = word;
-				}
-		}
-
-	}
-	 
 	 
 	public static boolean isPalindrome (String word){
 	    int i1 = 0;
 	    int i2 = word.length() - 1;
 	    while (i2 > i1) {
 	        if (word.charAt(i1) != word.charAt (i2)) {
-	            return false;
-	            		
+	            return false; 		
 	        }
 	        i1++;
 	        i2--;
 	    }
-	    
 	    return true;
-	    		
-	    
-	 
 	}
 	 }
 
