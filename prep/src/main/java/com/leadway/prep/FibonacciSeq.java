51bae1ffb2a2ba0a6482d78a25bfe33249a096e1
olvitole <olvitole@gmail.com>
2016-07-11 14:03:44 -0400

    Added new exercise

---
+++ prep/src/main/java/com/leadway/prep/FibonacciSeq.java
@@ -0,0 +1,20 @@
+package com.leadway.prep;
+
+/*
+ Write a function that generates the n th element of the fibonacci sequence
+ 
+ create the main method that iterates from 0 to TERMS and prints them all 
+ by calling the function that you have developed
+ 
+ */
+
+public class FibonacciSeq {
+	
+	private static int TERMS = 15; 
+	
+	
+	
+	
+	
+
+}
