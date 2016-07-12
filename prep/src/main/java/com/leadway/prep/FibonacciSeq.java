d153fe2c7dd47a73bee92b4ca82e703670c8de9c
ershelaruvina <ershelaruvina@192.168.1.13>
2016-07-12 14:27:02 -0400

    The content of the Fibonnaci Series class was updated.

--- prep/src/main/java/com/leadway/prep/FibonacciSeq.java
+++ prep/src/main/java/com/leadway/prep/FibonacciSeq.java
@@ -1,20 +1,29 @@
 package com.leadway.prep;
 
-/*
- Write a function that generates the n th element of the fibonacci sequence
- 
- create the main method that iterates from 0 to TERMS and prints them all 
- by calling the function that you have developed
- 
- */
+import java.util.Scanner;
 
 public class FibonacciSeq {
 	
 	private static int TERMS = 15; 
+	private static Scanner scnr;
 	
+	public static void main(String[] args) {
+		int num1;
+		scnr = new Scanner(System.in);
+		System.out.println("Start with the number 0:");
+		num1 = scnr.nextInt();
+		findNext(num1);
+	}
 	
-	
-	
-	
-
+	public static void findNext(int num) {
+		int[] fib = new int[TERMS];
+		fib[0] = num;
+		fib[1] = 1;
+		System.out.println("The next Fibonnaci number is:" + fib[1]);
+		for (int i = 2; i < fib.length;) {
+			fib[i] = fib[i - 1] + fib[i - 2];
+			System.out.println("The next Fibonnaci number is:" + fib[i]);
+			i++;
+		}
+	}
 }
