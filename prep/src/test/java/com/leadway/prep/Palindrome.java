4b87c3b15cdc7c63cc59a347abbe29f2fec46ab8
olvitole <olvitole@gmail.com>
2016-07-12 12:05:42 -0400

    Test Example

---
+++ prep/src/test/java/com/leadway/prep/Palindrome.java
@@ -0,0 +1,43 @@
+package com.leadway.prep;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	private static Scanner scnr;
+
+	public static void main(String[] args) {
+		scnr = new Scanner(System.in);
+		String temp = "";
+		while (temp != "exit") {
+
+			System.out.println("Enter a word:");
+			String word = scnr.next();
+			if (isPalindrome(word) == true) {
+				System.out.println("'" + word + "'" + " is a palindrome!!");
+				temp = word;
+			} else {
+				System.out.println("'" + word + "'" + " is NOT a palindrome!!");
+				temp = word;
+
+			}
+		}
+
+	}
+
+	public static boolean isPalindrome(String word) {
+		int i1 = 0;
+		int i2 = word.length() - 1;
+		while (i2 > i1) {
+			if (word.charAt(i1) != word.charAt(i2)) {
+				return false;
+
+			}
+			i1++;
+			i2--;
+		}
+
+		return true;
+
+	}
+}
